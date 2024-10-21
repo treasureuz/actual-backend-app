fa78bf748b7becebdbb937f41417c6557e3cee18
Coffee Fuel <coffeefuel@MacBook-Pro-3.attlocal.net>
2024-10-21 12:25:40 -0500

    Hosting Good

---
+++ src/icons.js
@@ -0,0 +1,22 @@
+// src/icons.js
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { 
+  faReact, 
+  faGoogle, 
+  faGithub, 
+  faPython 
+} from '@fortawesome/free-brands-svg-icons';
+import { 
+  faDatabase, 
+  faRobot 
+} from '@fortawesome/free-solid-svg-icons';
+
+// Add all icons to the library so you can use them in your components
+library.add(
+  faReact,
+  faGoogle,
+  faGithub,
+  faPython,
+  faDatabase,
+  faRobot
+);
